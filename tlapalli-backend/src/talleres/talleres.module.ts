3ad3ddd6bb4f639ad9788014770f089c11a59a61
jesushernandezmonts <jesushernandezmonts@gmail.com>
2026-07-10 01:32:29 -0600

    fix: add GatewayModule to TalleresModule

--- tlapalli-backend/src/talleres/talleres.module.ts
+++ tlapalli-backend/src/talleres/talleres.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TalleresController } from './talleres.controller';
 import { TalleresService } from './talleres.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { GatewayModule } from '../gateway/gateway.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, GatewayModule],
   controllers: [TalleresController],
   providers: [TalleresService]
 })
